c40f055e0fec39bc20785f660f8e8f95a33b3a8f
hidariutiwa <hidariutiwa22@gmail.com>
2022-12-03 20:35:03 +0900

    dd

---
+++ public/js/hoge.js
@@ -0,0 +1,7 @@
+document.getElementById('back-one').addEventListener('click', () => {
+    document.getElementById('hoge').innerHTML = 'こんにちは！';
+});
+
+document.getElementById('back-top').addEventListener('click', () => {
+    document.getElementById('hoge').innerHTML = 'こんばんは！';
+})
