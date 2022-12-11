e3e78c0833107a85c5789cc7db1f0e1fc2f2d6ae
hidariutiwa <hidariutiwa22@gmail.com>
2022-12-12 00:30:56 +0900

    ランダムレスポンス

--- public/js/hoge.js
+++ public/js/hoge.js
@@ -1,5 +1,5 @@
 const chatTextBox = document.getElementById('chat-textbox');
-const balloon = document.getElementById('chat-table-body');
+const chatTable = document.getElementById('chat-table-body');
 const sendButton = document.getElementById('send-button');
 
 sendButton.onclick = () => {
@@ -8,6 +8,25 @@ sendButton.onclick = () => {
     if(userText === '') {
         return;
     }
-    balloon.innerHTML += `<tr><td><div class="mycomment"><p>${userText}</p></div></td></tr>`;
+    chatTable.innerHTML += `<tr><td><div class="mycomment"><p>${userText}</p></div></td></tr>`;
     chatTextBox.value = '';
+
+    const resNum = Math.floor(Math.random() * 10);
+    console.log(resNum);
+    const response = responseMaster[resNum];
+    console.log(response);
+    chatTable.innerHTML += `<tr><td><div class="balloon6"><div class="faceicon"><img src="./img/chef.png" alt=""></div><div class="says"><p>${response}</p></div></div></td></tr>`;
+}
+
+const responseMaster = {
+    0: 'やっちゃん',
+    1: 'なんかようかい',
+    2: '何食べる？',
+    3: '何食べたい？',
+    4: 'よく考えて',
+    5: 'シモン',
+    6: 'どーするー',
+    7: 'お寿司希望ーー',
+    8: '焼肉行こー',
+    9: 'タイ料理食べタイ'
 }
