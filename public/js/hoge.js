76289fa862a4a8f430088ceacfc678999ff6952a
hidariutiwa <hidariutiwa22@gmail.com>
2022-12-11 19:26:05 +0900

    UI変更

--- public/js/hoge.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('back-one').addEventListener('click', () => {
-    document.getElementById('hoge').innerHTML = 'こんにちは！';
-});
-
-document.getElementById('back-top').addEventListener('click', () => {
-    document.getElementById('hoge').innerHTML = 'こんばんは！';
-})
