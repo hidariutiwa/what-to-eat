611bdce4d700cbba72a8b10cb16067af84f40686
hidariutiwa <hidariutiwa22@gmail.com>
2022-12-11 21:49:31 +0900

    LINE風

---
+++ public/js/hoge.js
@@ -0,0 +1,9 @@
+const chatTextBox = document.getElementById('chat-textbox');
+const balloon = document.getElementById('chat-table-body');
+const sendButton = document.getElementById('send-button');
+
+sendButton.onclick = () => {
+    //console.log('onClick');
+    const userText = chatTextBox.value;
+    balloon.innerHTML += `<tr><td><div class="mycomment"><p>${userText}</p></div></td></tr>`
+}
